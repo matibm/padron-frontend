1f4028d8dad2fc3df9845e2d0548eae3fc6bb95c
mechi <mechigrecogon93@gmail.com>
2021-02-04 01:28:37 -0300

    Fin Seccion 8

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -15,6 +15,8 @@ import { PagesComponent } from './pages.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { from } from 'rxjs';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 
@@ -27,6 +29,8 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     Grafica1Component,
     PagesComponent,
     AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent,
   ],
   
   exports: [
