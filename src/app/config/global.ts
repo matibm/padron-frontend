cdf51d55219e310fe927134431b3f1c26831fcfc
matibm <kewel2010.mb@gmail.com>
2021-05-24 11:49:56 -0400

    k

--- src/app/config/global.ts
+++ src/app/config/global.ts
@@ -1,6 +1,6 @@
 // import { URL_ENVIRONMENT } from './../../environments/environment';
 
 // export const URL_SERVICIOS = URL_ENVIRONMENT
-    export const URL_SERVICIOS = 'https://padron.mburgos.xyz'
+    export const URL_SERVICIOS = 'http://padron.mburgos.xyz'
 // export const URL_SERVICIOS = 'http://localhost:4000'
 // export const URL_SERVICIOS = 'http://192.168.1.63:4000'
